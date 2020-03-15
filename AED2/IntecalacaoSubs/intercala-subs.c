6cc965110611dc3e6b68783f254a20928b65d978
Davi Costa <davi.machado.costa@hotmail.com>
2020-03-15 12:47:55 -0300

    Inserindo código de João, comentando o código e mudando nomes de variáveis

--- AED2/IntecalacaoSubs/intercala-subs.c
+++ AED2/IntecalacaoSubs/intercala-subs.c
@@ -3,8 +3,6 @@
 #include <string.h>
 #include <conio.h>
 
-FILE *fita[3];
-
 void swap(char *xp, char *yp)
 {
 	char temp = *xp;
@@ -21,59 +19,60 @@ void bubbleSort(char arr[], int n)
 				swap(&arr[j], &arr[j + 1]);
 }
 
-char *geraBloco(char *str, char *lastHeap, int *numHeap)
+char *geraBloco(char *str, char *lastHeap, unsigned int *posChStr)
 {
 	//por medidas de seguranca, se o numero passado por parametro for maior que o tamanho da string a funcao retorna NULL
-	if (*numHeap > strlen(str))
+	if (*posChStr > strlen(str))
 		return NULL;
 	char heap[4], bloco[28];
-	int i, j, k = 0, qntdMarcados = 0;
+	int posChHeap, posChBloco, qntdMarcados = 0;
 	//caso essa funcao seja chamada pela primeira vez, o heap vai ser as tres primeiras letras da frase
-	if (*numHeap == 0)
+	if (*posChStr == 0)
 	{
-		for (i = 0; strlen(str) > *numHeap && i < 3; (*numHeap)++)
-			heap[i++] = str[*numHeap];
-		heap[i] = '\0';
+		for (posChHeap = 0; strlen(str) > *posChStr && posChHeap < 3; (*posChStr)++)
+			heap[posChHeap++] = str[*posChStr];
+		heap[posChHeap] = '\0';
 	}
 	//caso essa funcao nao seja chamada pela primeira vez, o heap vai ser uma string composta pelos caracteres marcados na ultima ordenacao
 	else
 		strcpy(heap, lastHeap);
+	posChHeap = 0;
 	//essa funcao preenche o bloco de forma ordenada
-	for (j = 0; qntdMarcados < 3 && k < 3; j++)
+	for (posChBloco = 0; qntdMarcados < 3 && posChHeap < 3; posChBloco++)
 	{
 		bubbleSort(heap, strlen(heap));
-		bloco[j] = heap[k];
-		if (strlen(str) > *numHeap)
+		bloco[posChBloco] = heap[posChHeap];
+		if (strlen(str) > *posChStr)
 		{
 			//caso a proxima letra da frase seja maior que a ultima letra do bloco ele troca a primeira letra do heap pela letra da frase
-			if (str[*numHeap] >= bloco[j])
-				heap[0] = str[*numHeap];
+			if (str[*posChStr] >= bloco[posChBloco])
+				heap[0] = str[*posChStr];
 			//caso a proxima letra da frase seja menor que a ultima letra do bloco ele armazena a letra na variavel lastHeap e aumenta o numero de letras marcadas
 			else
 			{
-				lastHeap[qntdMarcados] = str[*numHeap];
+				lastHeap[qntdMarcados] = str[*posChStr];
 				heap[0] = 126;
 				qntdMarcados++;
 			}
 		}
 		else
-			k++;
-		(*numHeap)++;
+			posChHeap++;
+		(*posChStr)++;
 	}
 	lastHeap[qntdMarcados] = '\0';
-	bloco[j] = '\0';
+	bloco[posChBloco] = '\0';
 	return bloco;
 }
 
-void abreArqs(char *modo)
+void abreFitas(FILE *fitas[], char *modo)
 {
-	int i = 1;
+	int numFita = 1;
 	char str[20];
-	for (i = 1; i < 4; i++)
+	for (numFita = 1; numFita < 4; numFita++)
 	{
-		sprintf(str, "file%d.txt", i);
-		fita[i - 1] = fopen(str, modo);
-		if (fita[i - 1] == NULL)
+		sprintf(str, "fita%d.txt", numFita);
+		fitas[numFita - 1] = fopen(str, modo);
+		if (fitas[numFita - 1] == NULL)
 		{
 			printf("Error: the file could not be opened\n");
 			printf("Closing the system...\n");
@@ -82,109 +81,56 @@ void abreArqs(char *modo)
 	}
 }
 
-void fechaArqs()
+void fechaFitas(FILE *fitas[])
 {
-	int i;
-	for (i = 0; i < 3; i++)
+	int numFita;
+	for (numFita = 0; numFita < 3; numFita++)
 	{
-		fclose(fita[i]);
-	}
-}
-
-void ordenaFinal()
-{
-	int j, i, k, l, pointer[3] = {0, 0, 0}, fitaMenorCh = 2;
-	char menorCh = 'z', ch;
-	rewind(fita[0]);
-	for (j = 0; j < 27; j++)
-	{
-		menorCh = 'z';
-		for (i = 0; i < 3; i++)
-		{
-			if (pointer[i] < 9)
-			{
-				fseek(fita[i], pointer[i], SEEK_SET);
-				ch = fgetc(fita[i]);
-				if (ch <= menorCh)
-				{
-					menorCh = ch;
-					fitaMenorCh = i;
-				}
-			}
-		}
-		pointer[fitaMenorCh]++;
-		if ((menorCh >= 'a' && menorCh <= 'z') || (menorCh >= 'A' && menorCh <= 'Z'))
-
-			fputc(menorCh, fita[0]);
-	}
-}
-
-void intercalaFitas()
-{
-	int j, i, k, l, pointer[3] = {0, 0, 0}, fitaMenorCh = 2, initialPointer = 0;
-	char menorCh = 'z', ch;
-	for (k = 0; k < 3; k++)
-	{
-		for (j = 0; j < 9; j++)
-		{
-			menorCh = 'z';
-			for (i = 0; i < 3; i++)
-			{
-				if (pointer[i] < initialPointer + 3)
-				{
-					fseek(fita[i], pointer[i], SEEK_SET);
-					ch = fgetc(fita[i]);
-					if (ch <= menorCh)
-					{
-						menorCh = ch;
-						fitaMenorCh = i;
-					}
-				}
-			}
-			pointer[fitaMenorCh]++;
-			if ((menorCh >= 'a' && menorCh <= 'z') || (menorCh >= 'A' && menorCh <= 'Z'))
-				fputc(menorCh, fita[k]);
-		}
-		initialPointer = initialPointer + 3;
-		for (l = 0; l < 3; l++)
-		{
-			pointer[l] = initialPointer;
-		}
+		fclose(fitas[numFita]);
 	}
 }
 
 int main()
 {
-	char chLido, frase[28], bloco[28], lastHeap[4] = "";
-	int i = 0;
-	abreArqs("w+");
+	FILE *fita[3];
+	char chLido, frase[28], bloco[10][28], lastHeap[4] = "";
+	unsigned int posChFrase = 0, numFita = 0, numBloco, qntdBlocos = 0;
+	abreFitas(fita, "w+");
 	printf("Querido usuario, por favor digite um conjunto de 27 caracteres: \n");
 	while (1)
 	{
+		//kbhit é uma funcao da biblioteca conio.h que retorna true toda vez que o usuario pressiona uma tecla no teclado
 		if (kbhit)
 		{
 			chLido = getch();
+			//caso o usuario pressione a tecla ENTER ele sai do loop
 			if (chLido == 13)
 				break;
-			if (i < 27)
+			//enquanto a quantidade de caracteres que o usuario digita é menor que 27, o sistema printa na tela a tecla e armazena na variavel frase
+			if (posChFrase < 27)
 			{
 				printf("%c", chLido);
-				frase[i] = chLido;
-				i++;
+				frase[posChFrase] = chLido;
+				posChFrase++;
 			}
 		}
 	}
-	frase[i] = '\0';
+	frase[posChFrase] = '\0';
 	printf("\n");
-	i = 0;
-	while (i <= strlen(frase))
+	posChFrase = 0;
+	//gera os blocos e armazena cada um na variavel bloco
+	for (qntdBlocos = 0; posChFrase <= strlen(frase); qntdBlocos++)
+		strcpy(bloco[qntdBlocos], geraBloco(frase, lastHeap, &posChFrase));
+	//insere metade dos blocos no primeiro arquivo e a outra metade no segundo
+	for (numBloco = 0; numBloco < qntdBlocos; numBloco++)
 	{
-		strcpy(bloco, geraBloco(frase, lastHeap, &i));
-		fputs(bloco, fita[0]);
-		fputc(' ', fita[0]);
+		if (numBloco == (qntdBlocos / 2) + (qntdBlocos % 2))
+			numFita++;
+		fputs(bloco[numBloco], fita[numFita]);
+		fputc(' ', fita[numFita]);
 	}
-	fechaArqs();
+	fechaFitas(fita);
 	return 0;
 }
 
-// frase de teste: INTERCALACAOBALANCEADA
+//frase de teste: INTERCALACAOBALANCEADA
